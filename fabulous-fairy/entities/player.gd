b061ed33ab726ba37d00ed0b0dc6ebe55dc28620
LealLara <motionverso0@gmail.com>
2026-05-23 08:04:33 -0300

    map

--- fabulous-fairy/entities/player.gd
+++ fabulous-fairy/entities/player.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 30.0
+const JUMP_VELOCITY = -300.0
 
 
 func _physics_process(delta: float) -> void:
